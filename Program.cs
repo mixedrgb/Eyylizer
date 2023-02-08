15cf628dfe7b1eef137d2161c3d45438fd0bbc3a
MixedRGB <test>
2023-02-08 14:56:14 -0600

    this doesnt work lmao

--- Program.cs
+++ Program.cs
@@ -2,20 +2,41 @@ using System;
 
 namespace Eyylizer
 {
+    public class Eyylizer
+    {
+        private const int MaxSpaces = 10000;
+        private const string Ey = "ey";
+	private string _theBigEyy = "";
+	public string GetEyy :
+
+        public string Build()
+	{
+            Random randNum = new Random();
+
+            for (int i = 0; i < MaxSpaces; i++)
+	    {
+                _theBigEyy += Ey;
+                for (int z = randNum.Next(1,9);z>0;z--)
+                    _theBigEyy += " ";
+	    }
+
+
+	    return _theBigEyy;
+	}
+
+	public void Display()
+	{
+            Eyylizer e = new Eyylizer();
+            Console.WriteLine(e.GetEyy);
+	}
+    }
+
     public class Program
     {
         internal static void Main()
         {
-            Random r = new Random();
-            for (int i = 0;i<10000;i++)
-            {
-                string s = "";
-                for (int z = r.Next(1,9);z>0;z--)
-                    s += " ";
-
-                Console.Write($"ey{s}");
-            }
-            Console.WriteLine();
+	    Eyylizer e = new Eyylizer();
+	    e.Display();
         }
     }
 }
